da22cc1c17a7636723dc3497962ec6a7888647bc
Lamtorne <endarman2424@gmail.com>
2026-01-26 01:32:41 +0300

    Main page blocks update

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import Link from "next/link";
 import Image from "next/image";
-import Feature from "@/app/components/Features"
 
 
 export default function HomePage() {
@@ -44,8 +43,8 @@ export default function HomePage() {
                 className="Pig-Picture"
                 src="/Pig.svg"
                 alt="Pig picture"
-                width={60}
-                height={60}
+                width={100}
+                height={100}
                 priority
               />
             </div>
@@ -64,8 +63,8 @@ export default function HomePage() {
                 className="Arrow-Picture"
                 src="/Arrow.svg"
                 alt="Arrow picture"
-                width={60}
-                height={60}
+                width={100}
+                height={100}
                 priority
               />
             </div>
@@ -84,8 +83,8 @@ export default function HomePage() {
                 className="Planet-Picture"
                 src="/Planet.svg"
                 alt="Planet picture"
-                width={60}
-                height={6}
+                width={100}
+                height={100}
                 priority
               />
             </div>
@@ -136,6 +135,189 @@ export default function HomePage() {
               height={500}
               priority
             />
+          </div>
+        </div>
+      </div>
+
+      <div className="Block-4">
+        <div className="Block-4-Header">
+          <h2>
+            Как это работает?
+          </h2>
+        </div>
+        <div className="Block-4-Cards-Container">
+          <div className="Block-4-SubBlock">
+            <div className="Block-4-List">
+              <p>1</p>
+            </div>
+            <div className="Block-4-SubText">
+              <h2>
+                Сбор данных
+              </h2>
+              <p>
+                Получаем мульти- и гиперспектральные снимки с российского спутника "Лобачевский" и агрохимическиие данные почв.
+              </p>
+            </div>
+          </div>
+
+          <div className="Block-4-SubBlock">
+            <div className="Block-4-List">
+              <p>2</p>
+            </div>
+            <div className="Block-4-SubText">
+              <h2>
+                Кластерный анализ
+              </h2>
+              <p>Алгоримы машинного обучения автоматически выделяют зоны с разными потребностями в удобрениях.
+              </p>
+            </div>
+
+          </div>
+
+          <div className="Block-4-SubBlock">
+            <div className="Block-4-List">
+              <p>3</p>
+            </div>
+            <div className="Block-4-SubText">
+              <h2>
+                Расчёт доз
+              </h2>
+              <p>
+                Рассчитываем оптимальные дозы удобрений для каждой зоны на основе данных о почве.
+              </p>
+            </div>
+          </div>
+
+          <div className="Block-4-SubBlock">
+            <div className="Block-4-List">
+              <p>4</p>
+            </div>
+            <div className="Block-4-SubText">
+              <h2>
+                Карта агрозондирования
+              </h2>
+              <p>
+                Вы получаете цифровую карту с рекомендациями, совместимую с системами точного земледелия.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="Block-5">
+        <h2 className="Block-5-Header">
+          Наши преимущества
+        </h2>
+
+        <div className="Block-5-Cards-Container">
+          <div className="Block-5-SubBlock-1">
+            <div className="Block-5-Blur">
+              <h2>
+                Анализ с отечественного спутника «Лобачевский»
+              </h2>
+              <p>
+                В отличие от аналогов наш сервис работает с данными высокого спектрального и пространственного разрешения, что позволяет детальнеее отслеживать состояние посевов.
+              </p>
+            </div>
+          </div>
+
+          <div className="Block-5-SubBlock-2">
+            <div className="Block-5-Blur">
+              <h2>
+                Комплексный подход
+              </h2>
+              <p>
+                Мы объединяем данные дистанционного зондирования Земли (ДЗЗ) с реальными агрохимическими характеристиками почв конкретного хозяйства.
+              </p>
+            </div>
+          </div>
+
+          <div className="Block-5-SubBlock-3">
+            <div className="Block-5-Blur">
+              <h2>
+                Совместимость
+              </h2>
+              <p>
+                Результаты экспортируются в виде цифровых карт, полностью совместимых с современными системами точного земледелия для дифференцированного внесения.
+              </p>
+            </div>
+          </div>
+
+          <div className="Block-5-SubBlock-4">
+            <div className="Block-5-Blur">
+              <h2>
+                Автоматизация через ML
+              </h2>
+              <p>
+                Использоваание современных алгоритмов машинного обучения и кластерного анализа позволяет выделять агрозоны автоматически, без необходимости ручной настройки агрономом.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="Block-6">
+        <h2 className="Block-6-Header">
+          Для кого
+        </h2>
+        <div className="Block-6-Cards-Container">
+          <div className="Block-6-SubBlock">
+            <div className="Block-6-Icon-Wrapper">
+              <Image
+                className="Truck-Picture"
+                src="/Truck.svg"
+                alt="Truck picture"
+                width={100}
+                height={100}
+                priority
+              />
+            </div>
+
+            <p className="Block-6-SubHeader">
+              Перерасход ресурсов
+            </p>
+            <p className="Block-6-SubText">
+              Усреднённые нормы приводят к переизбытку удобрений на одних участках и нехватке на других
+            </p>
+          </div>
+
+          <div className="Block-6-SubBlock">
+            <div className="Block-6-Icon-Wrapper">
+              <Image
+                className="Bank-Picture"
+                src="/Bank.svg"
+                alt="Bank picture"
+                width={100}
+                height={100}
+                priority
+              />
+            </div>
+
+            <p className="Block-6-SubHeader">
+              Снижение урожайности
+            </p>
+            <p className="Block-6-SubText">
+              Неравномерное удобрение растений снижает общую продуктивность поля
+            </p>
+          </div>
+
+          <div className="Block-6-SubBlock">
+            <div className="Block-6-Icon-Wrapper">
+              <Image
+                className="Atom-Picture"
+                src="/Atom.svg"
+                alt="Atom picture"
+                width={100}
+                height={100}
+                priority
+              />
+            </div>
+            <p className="Block-6-SubHeader">
+              Экологический вред
+            </p>
+            <p className="Block-6-SubText">
+              Избыток удобрений загрязняет близлежащие почву и водоемы
+            </p>
           </div>
         </div>
       </div>
