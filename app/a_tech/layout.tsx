305cdfdf8e68ec943104304196abb9ea6157804d
Lamtorne <endarman2424@gmail.com>
2026-02-26 19:32:28 +0300

    Changes

--- app/a_tech/layout.tsx
+++ app/a_tech/layout.tsx
@@ -1,5 +1,6 @@
 import '@/app/ui/global.css';
 import '@/app/components/header.css'
+import '@/app/ui/a-tech-page.css'
 import { righteous, inter, lusitana } from '../ui/fonts';
 import { Metadata } from 'next';
 import Header from '@/app/components/header.tsx'
--- app/a_tech/page.tsx
+++ app/a_tech/page.tsx
@@ -1,10 +1,29 @@
 import Link from "next/link";
 import Image from "next/image";
 
-export default function A_TechPage(){
+export default function A_TechPage() {
     return (
-         <main className="PageBackground">
-            <h1 className="text">second page</h1>
+        <main className="PageBackground">
+            <div className="Block-1">
+                <Image
+                    className="Satellite-Picture"
+                    src="/satellite.svg"
+                    alt="Satellite picture"
+                    width={700}
+                    height={500}
+                    priority
+                />
+                <div className="Block-1-Text">
+                    <div className="Block-1-Text-Blur">
+                        <h2>Технологическая основа Clusterlab</h2>
+
+                        <p>
+                            Наш сервис интегрирует мультиспектральные и гиперспектральные данные спутника "Лобачевский" и сервиса от ИКИ РАН "ВЕГА-Science" с алгоритмами машинного обучения для автоматизированного агрозонирования и оптимизации внесения минеральных удобрений.
+                        </p>
+
+                    </div>
+                </div>
+            </div>
         </main>
     );
 }
