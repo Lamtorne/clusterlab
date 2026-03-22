818f300d0da5b769e8afda729f36694c310f4091
Lamtorne <endarman2424@gmail.com>
2026-03-23 01:29:56 +0300

    Changes

--- app/(main)/components/header.tsx
+++ app/(main)/components/header.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
-import '@/app/(ncomponents/header.css'
+import '@/app/(main)/components/header.css'
 const Header = () => {
   return (
     <header className="Header-Header">
