92d17d53cac4779062504b7013271f7ef9cee73e
Arina <arinasmakh@gmail.com>
2026-04-01 18:15:01 +0300

    change

--- backend/config.py
+++ backend/config.py
@@ -1,7 +1,14 @@
 import os
+from pathlib import Path
 from dotenv import load_dotenv
 
+# Находим путь к папке, где лежит этот файл config.py
+env_path = Path(__file__).parent / ".env"
+load_dotenv(dotenv_path=env_path)
 
-load_dotenv()
 SECRET_KEY = os.getenv("SECRET_KEY")
+if not SECRET_KEY:
+    # Если всё равно None, выдаем ошибку сразу при запуске, а не при логине
+    raise ValueError("SECRET_KEY не найден! Проверь файл .env")
+
 ALGORITHM = "HS256"
