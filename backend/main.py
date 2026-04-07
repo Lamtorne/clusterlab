135bd18e18c5dc6861c3c43f39a79520c4f4c443
Arina <arinasmakh@gmail.com>
2026-04-08 01:20:05 +0300

    fields router

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from backend.routers import users
+from backend.routers import users, fields
 
 app = FastAPI(
     title='ClusterLab',
