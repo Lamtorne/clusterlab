47d24ae97fe1f73d382b36c553785af66eb4c9c2
Arina <arinasmakh@gmail.com>
2026-07-29 21:52:13 +0300

    changes in field results

--- backend/routers/fields.py
+++ backend/routers/fields.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException, status, BackgroundTasks
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
-
+import asyncio
 from datetime import datetime
 from decimal import Decimal
 
