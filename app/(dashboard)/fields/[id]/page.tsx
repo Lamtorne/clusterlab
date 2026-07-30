d2b8c5a72895b3027be5119515f4e93d0de1ea0e
Arina <arinasmakh@gmail.com>
2026-07-30 23:14:16 +0300

    added functions for LLM

--- app/(dashboard)/fields/[id]/page.tsx
+++ app/(dashboard)/fields/[id]/page.tsx
@@ -158,7 +158,6 @@ export default function FieldResultPage() {
             )}
           </div>
         </div>
-
       </div>
     </main>
   );
---
+++ backend/models/field_recommendation.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Integer, ForeignKey, JSON, DateTime, func, String
+from sqlalchemy.orm import mapped_column, Mapped
+from backend.database import Base
+from datetime import datetime
+
+
+class FieldRecommendation(Base):
+    __tablename__ = 'field_recommendations'
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    field_id: Mapped[int] = mapped_column(ForeignKey("fields.id"), nullable=False)
+    zones_rec: Mapped[list] = mapped_column(JSON, nullable=False)  # список зон с рекомендациями
+    short_zone_rec: Mapped[list] = mapped_column(String(150), nullable=False) # список кратких рекомендаций для визуализации
