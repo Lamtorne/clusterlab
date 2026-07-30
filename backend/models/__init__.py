ef530c796e556d76ca5a6ace13c24482ac6d4bea
Arina <arinasmakh@gmail.com>
2026-07-30 23:17:58 +0300

    try to build DB

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -1,5 +1,6 @@
 from .users import User
 from .fields import Field
+from .field_recommendation import FieldRecommendation
 
 
-__all__ = ["User", "Field"]
+__all__ = ["User", "Field", "FieldRecommendation"]
