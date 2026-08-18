fb6ab6676c6a9e9ede6848ffe7a868e4198139d5
Arina <arinasmakh@gmail.com>
2026-08-18 22:46:38 +0300

    edit auth

---
+++ backend/models/payments.py
@@ -0,0 +1,11 @@
+class Payment(Base):
+    __tablename__ = 'payments'
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)  # id платежа
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False) # user_id, кто будет платить
+    tariff: Mapped[str] = mapped_column(String(50), nullable=False)  # 'standard' / 'pro'
+    amount: Mapped[float] = mapped_column(Numeric(10, 2), nullable=False)  # сколько клиент заплатил
+    status: Mapped[str] = mapped_column(String(30), nullable=False)  # 'pending', 'succeeded', 'failed'
+    provider_payment_id: Mapped[str] = mapped_column(String(100), nullable=True)  # id платежа в ЮKassa
+    payment_method_id: Mapped[str] = mapped_column(String(100), nullable=True)  # токен для рекуррентных списаний
+    created_at: Mapped[datetime] = mapped_column(DateTime, server_default=func.now())
